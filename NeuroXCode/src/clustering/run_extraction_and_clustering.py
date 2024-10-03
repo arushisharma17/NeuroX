ca07d4dcf5c2c1db3e3d5e944a212d669f94c4dc
Akhilesh Nevatia <akhilesh.nevatia@gmail.com>
2024-10-02 22:09:59 -0500

    Changed setup.py to include an outline of clustering functionalities, and moved scripts not being used into archive, prevously built readme into archive

---
+++ NeuroXCode/src/clustering/run_extraction_and_clustering.py
@@ -0,0 +1,22 @@
+import argparse
+import os
+from .extract_activations import main as extract_activations
+from .process_and_cluster import main as process_and_cluster
+
+def main():
+    parser = argparse.ArgumentParser(description="Run extraction and clustering pipeline")
+    parser.add_argument("project_dir", type=str, help="Path to the project directory")
+    parser.add_argument("config_file", type=str, help="Path to the config file")
+    parser.add_argument("layer", type=int, help="Layer number")
+    parser.add_argument("clusters", type=int, help="Number of clusters")
+    parser.add_argument("clustering_methods", nargs="+", help="Clustering methods to use")
+    args = parser.parse_args()
+
+    # Run extraction
+    extract_activations(args.project_dir)
+
+    # Run clustering
+    process_and_cluster(args.project_dir, args.layer, args.clusters, args.clustering_methods)
+
+if __name__ == "__main__":
+    main()
