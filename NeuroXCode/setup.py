a73da7a3e4df05b83ffe6fb9da1368049e6e5ba3
Akhilesh Nevatia <akhilesh.nevatia@gmail.com>
2024-10-09 17:43:13 -0500

    Adding changes staged

--- NeuroXCode/setup.py
+++ NeuroXCode/setup.py
@@ -41,5 +41,28 @@ setup(
         'License :: OSI Approved :: MIT License',  # Adjust to your license
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',  # Adjust as per the minimum Python version your package supports
+    packages=find_packages(where="."),
+    install_requires=[
+        "h5py==3.6.0",
+        "imbalanced-learn==0.8.0",
+        "numpy>=1.21.0",
+        "scikit-learn>=1.0",
+        "scipy>=1.7.3",
+        "seaborn==0.11.1",
+        "svgwrite==1.4.1",
+        "transformers>=4.12.0",  # Update transformers to a compatible version
+        "tokenizers>=0.12.0",  # Ensure this is compatible
+        "torch>=2.0.0",
+        "matplotlib>=3.7.1",
+        "tqdm>=4.64.1",
+        "seaborn==0.11.1",
+        "dill==0.3.4",
+        "build==0.7.0",
+        "pytest==7.0.1",
+        "pytest-cov==3.0.0",
+        "sphinx==4.4.0",
+        "sphinx-book-theme==0.2.0",
+        "ufmt==1.3.2"
+    ],
+    python_requires=">=3.10",    
 )
