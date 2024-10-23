ac172ce92aaf644f47978cdc29914ebd985eb947
ManjulBalayar <90213041+ManjulBalayar@users.noreply.github.com>
2024-10-22 19:25:03 -0500

    Forgot to include alignment in init

--- NeuroXCode/src/NeuroXCode/__init__.py
+++ NeuroXCode/src/NeuroXCode/__init__.py
@@ -1,7 +1,8 @@
 from .algorithms.clustering import AgglomerativeClusteringPipeline, KMeansClusteringPipeline, LeadersClusteringPipeline
-
+from .evaluation import alignment
 __all__ = [
     "AgglomerativeClusteringPipeline",
     "KMeansClusteringPipeline",
-    "LeadersClusteringPipeline"
+    "LeadersClusteringPipeline",
+    "alignment"
 ]
